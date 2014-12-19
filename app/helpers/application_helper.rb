7e8074fbec106978af7a39e4776e34e2cc2ded12
mezbahalam <mezbahalam26@gmail.com>
2014-12-20 00:50:15 +0600

    flash notice design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_class(type)
+    case type
+    when :alert
+        "alert alert-error"
+    when :notice
+        "alert alert-success"
+    else
+        ""
+    end
+  end
 end
