bff66c7a3d86da7da8744d96378b2e37eb729c92
mezbahalam <mezbahalam26@gmail.com>
2015-01-01 22:36:28 +0600

    test profile name

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -27,11 +27,20 @@ class UserTest < ActiveSupport::TestCase
     end
 
   test "a user should have a profile name without spaces" do
-    user = User.new
+    user = User.new(first_name: 'Mezbah', last_name: 'Alam', email: 'mezbahalam27@gmail.com')
+    user.password = user.password_confirmation = '12121212'
+
     user.profile_name = "My pofile name with spaces"
     assert !user.save
     assert !user.errors[:profile_name].empty?
     assert user.errors[:profile_name].include?('Must be formatted correctly.')
   end
 
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'Mezbah', last_name: 'Alam', email: 'mezbahalam27@gmail.com')
+    user.password = user.password_confirmation = '12121212'
+
+    user.profile_name = 'mezbah_1'
+    assert user.valid?
+  end
 end
